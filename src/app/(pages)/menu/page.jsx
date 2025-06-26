c4943c557421fa879ec74049d7b9b9c240646078
MOOS <144733181+YahyaaMuhamedd@users.noreply.github.com>
2025-06-26 23:36:28 +0300

    fix

--- src/app/(pages)/menu/page.jsx
+++ src/app/(pages)/menu/page.jsx
@@ -1,10 +1,9 @@
-import { Title } from "@/ReusableComp/titles";
 
 export default function MenuPage() {
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <Title title="Menu" cssClasses="text-mainColor text-3xl mb-4" />
+            hi
         </div>
     );
 }
